d3fd993544b3f00f2c9e20a3dd7a50c19678d660
PEPE <PEPE@DESKTOP-QHHSDLU>
2022-01-20 18:04:50 +0100

    Tercera version 19/01/2022 con javadoc

--- src/main/java/Cuentas/Main.java
+++ src/main/java/Cuentas/Main.java
@@ -13,7 +13,6 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
         
-        
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
